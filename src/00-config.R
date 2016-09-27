bbc03aa9af4ed4e7f5f34471a2d8bcaf57419e14
Daniel Chen <Maveri2k@yahoo.com>
2016-09-26 20:27:04 -0400

    drop NA rows and convenience comment for debugging

--- src/00-config.R
+++ src/00-config.R
@@ -27,10 +27,12 @@ FULL_DATA <- '../test_data/WICER comprehension test.xlsx'
 #
 
 FULL_DATA_DF <- read_excel(FULL_DATA)
+FULL_DATA_DF <- FULL_DATA_DF[complete.cases(FULL_DATA_DF), ]
 all_ids <- FULL_DATA_DF[, 1]
 CK_PATTERN <- "_CK.?$"
 
 for (PT_ID in as.data.frame(all_ids)[, 1]) {
+    # PT_ID <- "Sample6"
     print(PT_ID)
     source('src/01-setup.R')
     source('src/02-correct_values.R')
